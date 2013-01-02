e3fffd85d7c8ad148fcdda6fa200d91637580283
dluter <dluter@gmail.com>
2013-01-02 21:22:21 +0800

    initialized commit.

---
+++ ubuntu/config/install.sh
@@ -0,0 +1,7 @@
+#install default configure for ubuntu
+echo "install default configure for ubuntu"
+
+cp ./bashrc $HOME/.bashrc
+cp ./minicom.dfl $HOME/.minicom.dfl
+cp ./gtktermrc $HOME/.gtktermrc
+cp ./gitconfig $HOME/.gitconfig
---
+++ ubuntu/setenv.sh
@@ -0,0 +1,13 @@
+#install ubuntu essential packages for development
+
+echo "update apt-get"
+sudo apt-get update;
+
+echo "install build-essential"
+sudo apt-get install build-essential
+
+echo "install cscope and ctags"
+sudo apt-get install  cscope ctags
+
+echo "install some useful packages"
+sudo apt-get install vim git git-core minicom stardict ia32-libs gtkterm smbfs libtool
